f9c001bcef1b78b945a74074a65b4ae2b0be2b2d
Maria Paula Sabogal Motta <mariathecharmix@hotmail.com>
2024-06-01 08:53:52 -0500

    Added configuration extension file

---
+++ Linktic.Ecommerce.ProductsCatalog/Linktic.Ecommerce.ProductsCatalog.Api/Extensions/ConfigurationExtension.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Linktic.Ecommerce.ProductsCatalog.Api.Extensions;
+
+public static class ConfigurationExtension
+{
+    private static LocalStackContainer? _localStackTestContainer;
+    private static LocalStackContainer LocalStackTestContainer => _localStackTestContainer ??= new LocalStackContainer();
+    
+    public static async Task ConfigureLocalStack(this IConfigurationBuilder configuration)
+    {
+        await LocalStackTestContainer.InitializeAsync();
+        
+        configuration.SetBasePath(System.AppContext.BaseDirectory)
+            .AddJsonFile("appsettings.json")
+            .AddEnvironmentVariables() 
+            .Build();
+
+        configuration.AddSystemsManager("/ecommerce", TimeSpan.FromSeconds(500));
+    }
+}
