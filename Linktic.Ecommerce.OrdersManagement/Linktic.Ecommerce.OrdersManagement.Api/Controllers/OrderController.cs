812105f670e83d2f80f8f38433441c8adc10a3c8
Maria Paula Sabogal Motta <mariathecharmix@hotmail.com>
2024-06-02 11:12:03 -0500

    Add and configure launchsettings.json

---
+++ Linktic.Ecommerce.OrdersManagement/Linktic.Ecommerce.OrdersManagement.Api/Controllers/OrderController.cs
@@ -0,0 +1,34 @@
+﻿using Linktic.Ecommerce.OrdersManagement.Business.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Serilog;
+
+namespace Linktic.Ecommerce.OrdersManagement.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class OrderController : ControllerBase
+{
+    private readonly IOrderService _orderService;
+
+    public OrderController(IOrderService orderService)
+    {
+        _orderService = orderService;
+    }
+    
+    [HttpGet]
+    public async Task<IActionResult> GetAllOrders()
+    {
+        try
+        {
+            var productsCatalog = await _orderService.GetAllOrders();
+
+            return Ok(productsCatalog);
+
+        }
+        catch (Exception e)
+        {
+            Log.Error(e,"{StackTrace} {Message}",e.StackTrace, e.Message);
+            return StatusCode(500);
+        }
+    }
+}
